74e82b9e5c85ba12a9ddbd37691fede2d57fea37
Tim Hall <thall74@gatech.edu>
2017-02-09 18:43:47 -0800

    Implemented through req# 13

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -62,26 +62,38 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
+        it('is hidden by default', function() {
+            expect(document.body.className).toContain("menu-hidden");
+        });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it("changes visibility when the menu icon is clicked", function() {
+          document.querySelector(".menu-icon-link").click();
+          expect(document.body.className).not.toContain("menu-hidden"); // before click, className contained menu-hidden
+
+          document.querySelector(".menu-icon-link").click();
+          expect(document.body.className).toContain("menu-hidden");
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+    });
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
+    
 }());
